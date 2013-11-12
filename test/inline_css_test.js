1ff3a33cb5d98905dbd15bda8be442895f2bb65c
Greg Allen <me@jga.me>
2013-11-12 08:30:12 -0800

    tests

--- test/inline_css_test.js
+++ test/inline_css_test.js
@@ -27,22 +27,14 @@ exports.inline_css = {
     // setup here if necessary
     done();
   },
-  default_options: function(test) {
+
+  basic: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    var actual = grunt.file.read('tmp/out.html');
+    var expected = grunt.file.read('test/expected/out.html');
+    test.equal(actual, expected, 'should inline css');
 
     test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
-
-    test.done();
-  },
+  }
 };
